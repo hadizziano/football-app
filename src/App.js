6e64bcd55bf921e25d759b9d05e7650f64210634
hadi <masimo.hadizzo@gmail.com>
2022-07-12 08:47:52 +0430

    some extra files removed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from "react";
 
-const App = () => {
-  return (
-    <h1>
-      Welcome this is a testto React App thats build using Webpack and Babel
-      separately
-    </h1>
-  );
+export const App = () => {
+  return <div>this is a react app</div>;
 };
 
 export default App;
